a352ff1c0cbba342bd986a6b4e49830f49110868
Px <42398474+PxPt@users.noreply.github.com>
2020-06-26 13:34:51 +0800

    commit  数据结构与算法 排序算法 -> 冒泡排序 | 选择排序 | 插入排序 ｜希尔排序　（交换法）

--- java/java-data-structure/src/main/java/com/runtime/algorithm/structure/sort/SortSummaryS.java
+++ java/java-data-structure/src/main/java/com/runtime/algorithm/structure/sort/SortSummaryS.java
@@ -65,28 +65,62 @@ public class SortSummaryS {
 
 
     //todo 时间复杂度 O(n2)
+    //todo 思想 : 将一个数组 抽象成两个数组 取 index ++ 位置的值 与 左侧数组中所有值进行比较
     public static void insertSort(int[] arr) {
 
-
+        int insertVal = 0;
+        int insertIndex = 0;
         for (int i = 1; i < arr.length; i++) {
-            int insertVal = arr[i];
-            int insertIndex = (i - 1);
+            insertVal = arr[i];
+            insertIndex = (i - 1);
 
-            while (insertIndex >= 0 && insertVal < arr[insertIndex]) { // insertIndex >= 0 && arr[1] < arr[0]
+            while (insertIndex >= 0 && insertVal > arr[insertIndex]) { // insertIndex >= 0 && arr[1] < arr[0]
                 arr[insertIndex + 1] = arr[insertIndex];
                 insertIndex--;
             }
-            arr[insertIndex + 1] = insertVal;
+            if (insertIndex + 1 != i) {
+                arr[insertIndex + 1] = insertVal;
+            }
+        }
+        //System.out.println(Arrays.toString(arr));
+    }
+
+
+    //todo
+    //todo
+    public static void shellSort(int[] arr) {
+        //[8, 9, 1, 7, 2,  3, 5, 4, 6, 0]
+        //[3, 5, 1, 6, 0,  8, 9, 4, 7, 2]
+        //[0, 2, 1, 4, 3,  5, 7, 6, 9, 8]
+
+
+        int temp = 0;
+
+        for (int log = arr.length / 2; log > 0; log /= 2) {
+
+            for (int i = log; i < arr.length; i++) {
+
+                for (int j = i - log; j >= 0; j -= log) {
+
+                    if (arr[j] < arr[j + log]) {
+
+                        temp = arr[j + log];
+                        arr[j + log] = arr[j];
+                        arr[j] = temp;
+                    }
+
+                }
+            }
         }
 
-        System.out.println(Arrays.toString(arr));
-
+         System.out.println(Arrays.toString(arr));
 
     }
 
+
     public static void main(String[] args) {
 
-        int[] arr = new int[80000];
+        int[] arr = new int[88888];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = (int) (Math.random() * 80000);
         }
@@ -95,8 +129,9 @@ public class SortSummaryS {
         System.out.println("开始时间: -> " + format.format(System.currentTimeMillis()));
         //bubbleSort(arr); //冒泡排序
         //selectSort(arr); //选择排序
-        int[] arr1 = {1, 3, 2, 215, 3125, 325325, 10};
-        insertSort(arr1);
+        //insertSort(arr); //插入排序
+        int[] arr1 = {8, 9, 1, 7, 2, 3, 5, 4, 6, 0};
+        shellSort(arr1);
 
         System.out.printf("结束时间: -> " + format.format(System.currentTimeMillis()));
 
